528c5516dfe4eceedbaf07e293b0a581c95ed3c3
Jonni <Jonni@DESKTOP-BTMTNEI>
2026-07-02 16:26:58 -0500

    Actualizar catalogo con nuevos productos y corregir nombres de imagen

--- src/data/productos.js
+++ src/data/productos.js
@@ -8,7 +8,42 @@ export const productos = [
   imagen: "/imagenes/productos/Jardin-violeta.png",
   disponible: true
 },
- 
+ {
+  id: 2,
+  nombre: "Ramo Carmesí",
+  descripcion: "Hermoso ramo artesanal elaborado con limpiapipas en vibrantes tonos rojos, combinado con delicadas margaritas blancas y elegantes detalles decorativos. Un regalo eterno que expresa amor, cariño y admiración, ideal para cualquier ocasión especial.",
+  precio: 15.00,
+  categoria: "Ramos Eternos",
+  imagen: "/imagenes/productos/Ramo-Carmesi.png",
+  disponible: true
+},
+{
+  id: 3,
+  nombre: "Tulipanes Rosados",
+  descripcion: "Hermoso ramo artesanal de tulipanes tejidos a mano con limpiapipas en delicados tonos rosados, acompañado de pequeños detalles decorativos y envuelto en un elegante papel decorativo. Un detalle eterno, tierno y perfecto para expresar amor, cariño y admiración en cualquier ocasión especial.",
+  precio: 15.00,
+  categoria: "Ramos Eternos",
+  imagen: "/imagenes/productos/tulipanes-rosados.png",
+  disponible: true
+},
+{
+  id: 4,
+  nombre: "Osito Graduado",
+  descripcion: "Tierno osito artesanal tejido a mano con limpiapipas, disponible por unidad y decorado con un elegante birrete de graduación. Un detalle único y duradero para celebrar logros académicos, felicitar a quienes alcanzan una nueva meta o sorprender con un recuerdo especial. Ideal para graduaciones de colegio, universidad o cualquier ocasión especial.",
+  precio: 10.00,
+  categoria: "Detalles Especiales",
+  imagen: "/imagenes/productos/osito-graduado.png",
+  disponible: true
+},
+{
+  id: 5,
+  nombre: "Margaritas Azules",
+  descripcion: "Hermoso ramo artesanal elaborado con limpiapipas en delicados tonos azules, celestes y blanco. Inspirado en la belleza de las margaritas, simboliza la tranquilidad, la esperanza y los momentos que permanecen en el corazón. Un detalle eterno, lleno de ternura y significado, perfecto para sorprender y expresar tus sentimientos en cualquier ocasión especial.",
+  precio: 18.00,
+  categoria: "Ramos Eternos",
+  imagen: "/imagenes/productos/margaritas-azules.png",
+  disponible: true
+},
 ];
 
 export default productos;
